93b9065f3f79e4a50fe897bc009c8b2161c27886
franzzy <francois.bosma@gmail.com>
2017-07-10 18:48:03 +0200

    [MISC] added a few changes to persistence.xml

--- server/src/dao/UserBlogDAO.java
+++ server/src/dao/UserBlogDAO.java
@@ -1,7 +1,18 @@
 package dao;
 
-/**
- * Created by franzzy on 10/07/17.
- */
+import beans.UserBlog;
+
+import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Dependent
 public class UserBlogDAO {
+    @Inject
+    private EntityManager em;
+
+    public List<UserBlog> findAll() {
+        return em.createQuery("FROM Student").getResultList();
+    }
 }
